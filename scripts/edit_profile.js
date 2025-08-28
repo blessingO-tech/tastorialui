497f01c571916f9bf58cd5f2c25d1d5f4d8966a9
blessingO-tech <blessingsolar76@gmail.com>
2025-08-28 20:30:13 +0100

    Update edit_profile.js

--- scripts/edit_profile.js
+++ scripts/edit_profile.js
@@ -24,7 +24,7 @@ $().ready(async function () {
         $('#aboutme').show()
         $('#bio').val(user.creator.bio || '');
     } else {
-        $('#aboutme').show()
+        $('#aboutme').hide()
         $('#bio').val('');
     }
 
